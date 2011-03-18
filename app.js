21578c01c71d41245f192d43fc5cf7559c83e268
Chinmay Garde <chinmaygarde@gmail.com>
2011-03-18 09:59:26 -0400

    Initial Commit

---
+++ app.js
@@ -0,0 +1,51 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express');
+
+var app = module.exports = express.createServer();
+
+// Configuration
+
+app.configure(function(){
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'ejs');
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(app.router);
+  app.use(express.static(__dirname + '/public'));
+});
+
+app.configure('development', function(){
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
+});
+
+app.configure('production', function(){
+  app.use(express.errorHandler()); 
+});
+
+// Routes
+
+app.get('/:user', function(req, res){
+  res.render('index', {
+    title: 'Mice',
+	user: req.params.user
+  });
+});
+
+var io = require('socket.io'); 
+var socket = io.listen(app); 
+socket.on('connection', function(client){
+	client.on('message', function(data){
+		socket.broadcast(data);
+	});
+});
+
+// Only listen on $ node app.js
+
+if (!module.parent) {
+  app.listen(3000);
+  console.log("Express server listening on port %d", app.address().port);
+}
